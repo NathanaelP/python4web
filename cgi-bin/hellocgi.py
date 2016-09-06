1c749e9a3e67e40e1c1d60657381fe5868965220
Nathanael John Potoski <dante2@dante2.info>
2016-09-05 18:48:39 -0600

    added cgi folder and a couple python scripts

---
+++ cgi-bin/hellocgi.py
@@ -0,0 +1,4 @@
+import sys
+sys.stdout.write("content-type: text/html \r\n\r\n")
+sys.stdout.write("<!DOCTYPE html><html><head><title>Hello CGI</title></head>")
+sys.stdout.write("<body><h2>Hello CGI</h2></body></html>")
