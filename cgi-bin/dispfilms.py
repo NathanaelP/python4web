fd73f23f8fda4a52c0152d5e552e0a61438ccf51
NathanaelP <dante2@dante2.info>
2016-09-11 13:10:55 -0600

    added sql server script

---
+++ cgi-bin/dispfilms.py
@@ -0,0 +1,18 @@
+import cgi, os, sys
+
+import sqlite3 as db
+
+conn = db.connect('test.db')
+cursor = conn.cursor()
+conn.row_factory = db.Row
+cursor.execute("select * from films")
+rows = cursor.fetchall()
+
+sys.stdout.write("content-type: text.html\r\n\r\n")
+sys.stdout.write("")
+sys.stdout.write("<html><body><p>")
+for row in rows:
+    sys.stdout.write("%s %s %s " % (row[0], row[1], row[2]))
+    sys.stdout.write("<br />")
+    
+sys.stdout.write(</p></body></html>)
